27813df2194a8b55e62426927e723f5bd9770372
babidi34 <karimbaidi@gmail.com>
2022-09-28 18:05:43 +0200

    gitlab public to github public

--- fonctions.py
+++ fonctions.py
@@ -2,6 +2,9 @@ from config import token_gitlab, token_github, gitlab_url, github_url, github_us
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+import tempfile
+from git import Repo
+
 
 
 def get_repos_gitlab():
@@ -36,4 +39,9 @@ def create_repo_github(nom_repo):
 def delete_repo_github(nom_repo):#not used
     delete_repo = requests.delete(f"https://api.github.com/repos/babidi34/{nom_repo}",auth = HTTPBasicAuth(github_username, token_github)).json()
 
-
+def cloneGitlab_and_pushGithub(repo):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        print('created temporary directory', tmpdirname)
+        cloned_repo = Repo.clone_from(repo['web_url'], tmpdirname)
+        remote = cloned_repo.create_remote("new", url=f"https://{github_username}:{token_github}@github.com/{github_username}/{repo['name']}.git")
+        remote.push(refspec='{}:{}'.format("master", "master"))
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import fonctions
 public_repo_gitlab = fonctions.get_repos_public_gitlab()
 private_repo_gitlab = fonctions.get_repos_private_gitlab()
 repos_github = fonctions.get_repos_github()
-fonctions.create_repo_github("mon_nouveau_repo_de_test")
 
-print(repos_github)
+for repo in public_repo_gitlab:
+    fonctions.create_repo_github(repo['name'])
+    fonctions.cloneGitlab_and_pushGithub(repo)
 
 
