57afa10a8d11bc836fbc1354dfab4577a9c58ecb
babidi34 <karimbaidi@gmail.com>
2022-10-15 00:14:15 +0200

    to relieve main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import fonctions, config
 
 public_repo_gitlab = fonctions.get_repos_public_gitlab()
 private_repo_gitlab = fonctions.get_repos_private_gitlab()
-repos_github = fonctions.get_repos_github()
 
 for repo in public_repo_gitlab:
     fonctions.create_repo_github(repo['name'])
